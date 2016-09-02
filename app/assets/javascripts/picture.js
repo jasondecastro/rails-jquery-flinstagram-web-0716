7f663e00724688edf7bb66883b452321c26baf6f
Jason Decastro <jasonrdecastro@gmail.com>
2016-09-02 16:47:36 -0400

    finished rails jquery flinstagram lab

--- app/assets/javascripts/picture.js
+++ app/assets/javascripts/picture.js
@@ -1,1 +1,13 @@
 // Add your JavaScript here
+
+$(function() {
+  addFormEventHandler();
+})
+
+function addFormEventHandler() {
+  $('.link').on('ajax:complete', function(e, request, status){
+    $('#picture-' + request.responseJSON.id).html(request.responseJSON.content);
+    $('.like-btn span#' + request.responseJSON.id).removeClass();
+    $('.like-btn span#' + request.responseJSON.id).addClass(request.responseJSON.heart);
+  })
+}
--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -5,9 +5,15 @@ class PicturesController < ApplicationController
   end
 
   def like
+    # binding.pry
+
     @picture = Picture.find(params[:id])
     @picture.update_likes(current_user)
-    redirect_to root_path
+
+    respond_to do |format|
+      format.html { redirect_to root_path }
+      format.json { render json: { content: @picture.likes_message(current_user), heart: @picture.heart_class(current_user), id: @picture.id}}
+    end
   end
 
 end
